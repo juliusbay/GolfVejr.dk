02a4c4de5499f4b747c9748b2217c775b6152bbc
JuliusBayAxe_7qoprnt <juax0001@stud.kea.dk>
2025-08-14 14:38:02 +0200

    -

--- src/main/java/com/example/golfvejr/Controller/ForecastController.java
+++ src/main/java/com/example/golfvejr/Controller/ForecastController.java
@@ -18,7 +18,7 @@ public class ForecastController {
 
 
     // Koordinater skal opdateres til en @requestParam og dropdown menu for hver golfklub. Pt. er koordinater København
-    @GetMapping("/golfvejr")
+    @GetMapping("/")
     public String getForecast(Model model) {
         CompleteForecast forecast = yrApiService.getForecast(60.10, 9.58);
         List<TimeSeries> timeEntries = forecast.getProperties().getTimeSeries();
