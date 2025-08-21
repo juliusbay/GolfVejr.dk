435e83b8012f29f5563d49d629a06272d63148ef
JuliusBayAxe_7qoprnt <juax0001@stud.kea.dk>
2025-08-21 12:50:56 +0200

    added formatting to hour and date display

--- src/main/java/com/example/golfvejr/Controller/ForecastController.java
+++ src/main/java/com/example/golfvejr/Controller/ForecastController.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
